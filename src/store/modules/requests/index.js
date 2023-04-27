17e8e32acde79bab132caa5a7040bcb2823cef12
tanerdzhemal <tanerdzhemal3@gmail.com>
2023-04-27 17:05:00 +0300

    requests logic

--- src/store/modules/requests/index.js
+++ src/store/modules/requests/index.js
@@ -1,3 +1,6 @@
+import mutations from './mutations.js';
+import actions from './actions.js';
+
 export default {
   namespaced: true,
   state() {
@@ -5,4 +8,6 @@ export default {
       requests: [],
     };
   },
+  mutations,
+  actions,
 };
