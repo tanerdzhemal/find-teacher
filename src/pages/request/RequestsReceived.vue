7370a43d23d145591e0f92d3ec3b2e0f53e91c12
tanerdzhemal <tanerdzhemal3@gmail.com>
2023-04-28 20:27:52 +0300

    asdasdqwe

--- src/pages/request/RequestsReceived.vue
+++ src/pages/request/RequestsReceived.vue
@@ -1,7 +1,57 @@
-<template>REQUESTS</template>
+<template>
+  <section>
+    <base-card>
+      <header>
+        <h2>Requests Received</h2>
+      </header>
+      <ul v-if="hasRequests">
+        <request-item
+          v-for="req in recievedRequests"
+          :key="req.id"
+          :email="req.userEmail"
+          :message="req.message"
+        >
+        </request-item>
+      </ul>
+      <h3 v-else>You haven't received any requests yet!</h3>
+    </base-card>
+  </section>
+</template>
 
 <script>
-export default {};
+import RequestItem from '../../components/requests/RequestItem.vue';
+export default {
+  components: {
+    RequestItem,
+  },
+  data() {
+    return {};
+  },
+  computed: {
+    recievedRequests() {
+      return this.$store.getters['requests/requests'];
+    },
+    hasRequests() {
+      return this.$store.getters['requests/hasRequests'];
+    },
+  },
+  methods: {},
+};
 </script>
 
-<style></style>
+<style scoped>
+header {
+  text-align: center;
+}
+
+ul {
+  list-style: none;
+  margin: 2rem auto;
+  padding: 0;
+  max-width: 30rem;
+}
+
+h3 {
+  text-align: center;
+}
+</style>
