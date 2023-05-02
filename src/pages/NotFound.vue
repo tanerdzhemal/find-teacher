a84818d86a04c5768b7887a2f4e21d0786212855
tanerdzhemal <tanerdzhemal3@gmail.com>
2023-05-02 20:22:13 +0300

    l

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,4 +1,14 @@
-<template>Page Not found</template>
+<template>
+  <section>
+    <base-card>
+      <h2>Page Not Found</h2>
+      <p>
+        This page could not be found - maybe checks out all our
+        <router-link to="/coaches">Coaches</router-link>.
+      </p>
+    </base-card>
+  </section>
+</template>
 
 <script>
 export default {};
