6eaf66382f7913b1d7d392fc0d3eda78f92ae38a
tanerdzhemal <tanerdzhemal3@gmail.com>
2023-05-04 16:46:32 +0300

    asd

--- src/pages/auth/UserAuth.vue
+++ src/pages/auth/UserAuth.vue
@@ -10,11 +10,21 @@
       <form @submit.prevent="submitForm">
         <div class="form-control">
           <label for="email">E-mail</label>
-          <input type="email" id="email" v-model.trim="email" />
+          <input
+            type="email"
+            id="email"
+            v-model.trim="email"
+            autocomplete="username"
+          />
         </div>
         <div class="form-control">
           <label for="password">Password</label>
-          <input type="password" id="password" v-model.trim="password" />
+          <input
+            type="password"
+            id="password"
+            v-model.trim="password"
+            autocomplete="current-password"
+          />
         </div>
         <p v-if="!formIsValid">
           Plesase enter a valid email and password(atleast 6 characters)!
